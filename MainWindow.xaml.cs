3a8c99f4a9e98d2c7c8f7e1dbf3ea1b413426399
Dave Thalmann <thalmann@sogedes.com>
2022-08-20 18:01:49 +0200

    Initial implementation

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+using NHotkey;
+using NHotkey.Wpf;
+
+namespace AutoClicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private Timer? _timer;
+        
+        public MainWindow()
+        {
+            HotkeyManager.Current.AddOrReplace("Increment", Key.F8, ModifierKeys.None, HotkeyAutoClicker);
+            InitializeComponent();
+        }
+
+        private void HotkeyAutoClicker(object? sender, HotkeyEventArgs e)
+        {
+            ToggleButtonAutoClick.IsChecked = ToggleButtonAutoClick.IsChecked != true;
+        }
+
+        private void ToggleButtonAutoClick_Checked(object sender, RoutedEventArgs e)
+        {
+            int interval = int.Parse(TextBoxInterval.Text);
+
+            if (interval == 0)
+            {
+                interval = 1;
+            }
+            
+            _timer = new Timer((object? state) =>
+            {
+                MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown | MouseOperations.MouseEventFlags.LeftUp);
+            }, null, new TimeSpan(0), new TimeSpan(TimeSpan.TicksPerMillisecond * interval));
+        }
+
+        private void ToggleButtonAutoClick_UnChecked(object sender, RoutedEventArgs e)
+        {
+            _timer?.Dispose();
+        }
+
+        private void TextBoxInterval_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+    }
+}
---
+++ MouseOperations.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace AutoClicker
+{
+
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    public static class MouseOperations
+    {
+        [Flags]
+        public enum MouseEventFlags
+        {
+            Move = 0x00000001,
+            LeftDown = 0x00000002,
+            LeftUp = 0x00000004,
+            RightDown = 0x00000008,
+            RightUp = 0x00000010,
+            MiddleDown = 0x00000020,
+            MiddleUp = 0x00000040,
+            Absolute = 0x00008000,
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetCursorPos(int x, int y);      
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetCursorPos(out MousePoint lpMousePoint);
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        public static void SetCursorPosition(int x, int y) 
+        {
+            SetCursorPos(x, y);
+        }
+
+        public static void SetCursorPosition(MousePoint point)
+        {
+            SetCursorPos(point.X, point.Y);
+        }
+
+        public static MousePoint GetCursorPosition()
+        {
+            bool gotPoint = GetCursorPos(out var currentMousePoint);
+
+            if (!gotPoint)
+            {
+                currentMousePoint = new MousePoint(0, 0);
+            }
+            
+            return currentMousePoint;
+        }
+
+        public static void MouseEvent(MouseEventFlags value)
+        {
+            var position = GetCursorPosition();
+
+            mouse_event
+                ((int)value,
+                    position.X,
+                    position.Y,
+                    0,
+                    0)
+                ;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public readonly struct MousePoint
+        {
+            public readonly int X;
+            public readonly int Y;
+
+            public MousePoint(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+    }
+}
